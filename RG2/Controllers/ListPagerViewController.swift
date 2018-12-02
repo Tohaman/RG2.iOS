011c9dac915f6ce526ebb63486f5cad63c8f1fc1
Rozov Anton <avrozovu@yandex.ru>
2018-12-02 13:02:50 +0300

    контроллер для этапа

---
+++ RG2/Controllers/ListPagerViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ListPagerViewController.swift
+//  RG2
+//
+//  Created by Anton on 01.12.2018.
+//  Copyright © 2018 RubicsGuide. All rights reserved.
+//
+
+import UIKit
+
+class ListPagerViewController: UIViewController {
+
+    @IBOutlet weak var titleImage: UIImageView!
+    @IBOutlet weak var titleText: UILabel!
+    @IBOutlet weak var descriptionText: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- RG2/Controllers/MainTableViewController.swift
+++ RG2/Controllers/MainTableViewController.swift
@@ -25,7 +25,9 @@ class MainTableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        //self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        
     }
 
     // MARK: - Table view data source
@@ -49,50 +51,28 @@ class MainTableViewController: UITableViewController {
         
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func showAlert (index: Int) {
+        let ac = UIAlertController(title: nil, message: "Выберите действие", preferredStyle: .actionSheet)
+        //let call = UIAlertAction(title: "Позвонить: \(index)", style: .default, handler: nil)
+        let call = UIAlertAction(title: "Позвонить: \(index)", style: .default) {
+            (action: UIAlertAction) -> Void in
+            let alertC = UIAlertController(title: nil, message: "Не могу позвонить", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertC.addAction(ok)
+            self.present(alertC, animated: true, completion: nil)
+        }
+        
+        
+        let cancel = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+        
+        ac.addAction(cancel)
+        ac.addAction(call)
+        present (ac, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //showAlert(index: indexPath.row)
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
