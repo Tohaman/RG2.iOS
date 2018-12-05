3b5890a254fc144cc0282284c6eadd662ebf4146
Rozov Anton <avrozovu@yandex.ru>
2018-12-05 23:38:25 +0300

    Try to setup YouTube Player

--- RG2/Controllers/ListPagerViewController.swift
+++ RG2/Controllers/ListPagerViewController.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
-import YoutubePlayer_in_WKWebView
+//import YoutubePlayer_in_WKWebView
+import youtube_ios_player_helper_swift
 
 class ListPagerViewController: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var titleImage: UIImageView!
     @IBOutlet weak var titleText: UILabel!
     @IBOutlet weak var descriptionText: UITextView!
-    @IBOutlet var ytPlayer: WKYTPlayerView!
+    @IBOutlet var ytPlayer: YTPlayerView! //WKYTPlayerView!
+    @IBOutlet weak var loadingMovieIndicator: UIActivityIndicatorView!
     
     var phase = ""
     var id = 0
@@ -65,10 +67,10 @@ class ListPagerViewController: UIViewController, UITextViewDelegate {
         
         // Load video from YouTube URL
         //let myVideoURL = NSURL(string: "https://www.youtube.com/watch?v=GNp6Xe5_tXU")
-        ytPlayer.backgroundColor = .black
+        //ytPlayer.backgroundColor = .black
         videoId = lp.url
-        ytPlayer.load(withVideoId: lp.url)
-        ytPlayer.cueVideo(byId: lp.url, startSeconds: 60, suggestedQuality: .default)
+        ytPlayer.delegate = self
+        ytPlayer.loadVideo(videoId: videoId)
         //ytPlayer.loadVideoURL(myVideoURL!)
     }
     
@@ -82,7 +84,7 @@ class ListPagerViewController: UIViewController, UITextViewDelegate {
             let time = stringUrl.substringAfter(char: "=").substringBefore(char: "&")
             let minutes = time.substringBefore(char: ":")
             let seconds = time.substringAfter(char: ":")
-            let timeInSec = (Float(minutes) ?? 0 * 60 ) + (Float(seconds) ?? 0)
+            let timeInSec = "\(Int(60 * (Float(minutes) ?? 0) + (Float(seconds) ?? 0)))"
             print ("VideID = \(videoID) min = \(minutes) sec = \(seconds) timeInSec = \(timeInSec)")
             
             if let keyWindow = UIApplication.shared.keyWindow {
@@ -97,8 +99,10 @@ class ListPagerViewController: UIViewController, UITextViewDelegate {
                 let yAnchor = keyWindow.frame.height/2 - (height / 2)
                 let videoPlayerFrame = CGRect(x: 10, y: yAnchor, width: keyWindow.frame.width-20, height: height)
 //                let videoPlayerView = YouTubePlayerView(frame: videoPlayerFrame)
-                let videoPlayerView = WKYTPlayerView(frame: videoPlayerFrame)
+                let videoPlayerView = YTPlayerView(frame: videoPlayerFrame)
                 videoPlayerView.delegate = self
+                videoPlayerView.loadVideo(videoId: videoID, startTime: timeInSec)
+                videoPlayerView.autoPlay = true
                 //let videoPlayerView = UIView(frame: videoPlayerFrame)
                 
                 //videoPlayerView.backgroundColor = .black
@@ -107,7 +111,7 @@ class ListPagerViewController: UIViewController, UITextViewDelegate {
                 view.addSubview (videoPlayerView)
                 //videoPlayerView.loadVideoID("l6V7517N_lQ")
                 //videoPlayerView.load(withVideoId: videoID)
-                videoPlayerView.loadVideo(byId: videoID, startSeconds: 0, suggestedQuality: .auto )
+                //videoPlayerView.loadVideo(byId: videoID, startSeconds: 0, suggestedQuality: .auto )
                 self.view.addSubview(view)
                 
                 let aSelector : Selector = #selector(self.removeSubview)
@@ -146,6 +150,8 @@ class ListPagerViewController: UIViewController, UITextViewDelegate {
      }
      */
     
+
+    
     @objc func removeSubview(){
         //print("Start remove sibview")
         if let viewWithTag = self.view.viewWithTag(101) {
@@ -157,7 +163,35 @@ class ListPagerViewController: UIViewController, UITextViewDelegate {
     
 }
 
-extension ListPagerViewController: WKYTPlayerViewDelegate {
+
+extension YTPlayerView {
+    
+    func loadVideo(videoId:String, startTime: String) {
+        //https://developers.google.com/youtube/player_parameters
+        let playerVars:[String: Any] = [
+            "controls" : "1",       //0 – элементы управления не отображаются, 1-2 - отображаются
+            "showinfo" : "1",       //начении 0 проигрыватель перед началом воспроизведения не выводит информацию о видео
+            "autoplay": "0",        //начинается ли воспроизведение исходного видео сразу после загрузки проигрывателя.
+            "rel": "0",             //будут ли воспроизводиться похожие видео после завершения показа исходного видео.
+            "modestbranding": "1",  //начение 1, чтобы логотип YouTube не отображался на панели управления
+            "iv_load_policy" : "3", // 1 аннотации видео по умолчанию будут отображаться, а при значении 3 – будут скрыты.
+            "fs": "1",              //1 отображает кнопку полноэкранного режима.
+            "playsinline" : "1",    //полноэкранный режим 1 - нет, 0 - да
+            "start" : startTime     //Начальное время в секундах
+        ]
+        _ = self.load(videoId: videoId, playerVars: playerVars)
+        //Если ставим свои кнопки play/pause, то можно использовать вместе с "controls" : "0"
+        //ytPlayer.isUserInteractionEnabled = false
+    }
+    
+    func loadVideo(videoId: String) {
+        self.loadVideo(videoId: videoId, startTime: "0")
+    }
+}
+
+
+//WKYTPlayerViewDelegate
+extension ListPagerViewController: YTPlayerViewDelegate  {
     
 //    func playerViewDidBecomeReady(_ playerView: WKYTPlayerView) {
 //        ytPlayer.playVideo(at: 0)
@@ -168,7 +202,26 @@ extension ListPagerViewController: WKYTPlayerViewDelegate {
 //        playerView.playVideo()
 //    }
     
-    func playerViewPreferredWebViewBackgroundColor(_ playerView: WKYTPlayerView) -> UIColor {
+//    func playerViewPreferredWebViewBackgroundColor(_ playerView: WKYTPlayerView) -> UIColor {
+//        return .clear
+//    }
+//
+    func playerViewDidBecomeReady(_ playerView: YTPlayerView){
+        loadingMovieIndicator.stopAnimating()
+        if playerView.autoPlay {
+            playerView.playVideo()
+        }
+//        btnPlayPause.backgroundColor = #colorLiteral(red: 0.370555222, green: 0.3705646992, blue: 0.3705595732, alpha: 0.5104880137)
+//        btnPlayPause.setImage(UIImage(named:"play_icon"), for:.normal)
+    }
+    
+    func playerViewPreferredWebViewBackgroundColor(_ playerView: YTPlayerView) -> UIColor{
         return .clear
     }
+    
+    func playerViewPreferredInitialLoadingView(_ playerView: YTPlayerView) -> UIView?{
+        return UIImageView(image:UIImage(named:"not_found"))
+    }
+
+    
 }
