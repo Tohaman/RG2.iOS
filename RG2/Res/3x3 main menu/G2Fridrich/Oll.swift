e83e5cbacdf637e909cca944f18d1850e3bebb37
Tohaman <avrozovu@yandex.ru>
2019-01-11 23:02:02 +0300

    Commit

---
+++ RG2/Res/3x3 main menu/G2Fridrich/Oll.swift
@@ -0,0 +1,141 @@
+//
+//  Oll.swift
+//  RG2
+//
+//  Created by Anton on 01.01.2019.
+//  Copyright © 2019 RubicsGuide. All rights reserved.
+//
+
+import Foundation
+
+struct Oll: Phases {
+    func phase() -> String {
+        let phase = "OLL"
+        return phase
+    }
+    
+    func titles() -> [String] {
+        return title
+    }
+    
+    func images() -> [String] {
+        return image
+    }
+    
+    func descs() -> [String] {
+        return description
+    }
+    
+    func urls() -> [String] {
+        return url
+    }
+    
+    func comments() -> [String] {
+        return []
+    }
+    
+    let title = [
+        st1_title,
+        st2_title,
+        st3_title,
+        st4_title,
+        st5_title,
+        st6_title,
+        st7_title,
+        st8_title,
+        st9_title,
+        st10_title
+    ]
+    
+    let image = [
+        "image_1",
+        "image_2",
+        "image_3",
+        "image_4",
+        "image_5",
+        "image_6",
+        "image_7",
+        "image_8",
+        "image_9",
+        "image_10"
+    ]
+    
+    let url = [
+        st1_url,
+        st2_url,
+        st3_url,
+        st4_url,
+        st5_url,
+        st6_url,
+        st7_url,
+        st8_url,
+        st9_url,
+        st10_url
+    ]
+    
+    
+    let description = [
+        st1_description,
+        st2_description,
+        st3_description,
+        st4_description,
+        st5_description,
+        st6_description,
+        st7_description,
+        st8_description,
+        st9_description,
+        st10_description
+    ]
+    
+    
+    static let st1_title = "1"
+    static let st1_description = """
+    """
+    static let st1_url = ""
+    
+    static let st2_title = "2"
+    static let st2_description = """
+    """
+    static let st2_url = ""
+    
+    static let st3_title = "3"
+    static let st3_description = """
+    """
+    static let st3_url = ""
+    
+    static let st4_title = "4"
+    static let st4_description = """
+    """
+    static let st4_url = ""
+    
+    static let st5_title = "5"
+    static let st5_description = """
+    """
+    static let st5_url = ""
+    
+    static let st6_title = "6"
+    static let st6_description = """
+    """
+    static let st6_url = ""
+    
+    static let st7_title = "7"
+    static let st7_description = """
+    """
+    static let st7_url = ""
+    
+    static let st8_title = "8"
+    static let st8_description = """
+    """
+    static let st8_url = ""
+    
+    static let st9_title = "9"
+    static let st9_description = """
+    """
+    static let st9_url = ""
+    
+    static let st10_title = "10"
+    static let st10_description = """
+    """
+    static let st10_url = ""
+    
+}
--- RG2/Res/3x3 main menu/Patterns.swift
+++ RG2/Res/3x3 main menu/Patterns.swift
@@ -22,7 +22,7 @@ struct Patterns: Phases {
     }
     
     func descs() -> [String] {
-        return description
+        return descr
     }
     
     func urls() -> [String] {
@@ -46,7 +46,10 @@ struct Patterns: Phases {
         st10_title,
         st11_title,
         st12_title,
-        st13_title
+        st13_title,
+        st14_title,
+        st15_title,
+        st16_title
     ]
     
     let image = [
@@ -62,7 +65,10 @@ struct Patterns: Phases {
         "pattern_10",
         "pattern_11",
         "pattern_12",
-        "pattern_13"
+        "pattern_13",
+        "pattern_14",
+        "pattern_15",
+        "pattern_16"
     ]
     
     let url = [
@@ -78,24 +84,30 @@ struct Patterns: Phases {
         st10_url,
         st11_url,
         st12_url,
-        st13_url
+        st13_url,
+        st14_url,
+        st15_url,
+        st16_url
     ]
     
     
-    let description = [
-        st1_description,
-        st2_description,
-        st3_description,
-        st4_description,
-        st5_description,
-        st6_description,
-        st7_description,
-        st8_description,
-        st9_description,
-        st10_description,
-        st11_description,
-        st12_description,
-        st13_description
+    let descr = [
+        st1_descr,
+        st2_descr,
+        st3_descr,
+        st4_descr,
+        st5_descr,
+        st6_descr,
+        st7_descr,
+        st8_descr,
+        st9_descr,
+        st10_descr,
+        st11_descr,
+        st12_descr,
+        st13_descr,
+        st14_descr,
+        st15_descr,
+        st16_descr
     ]
     
     let comment = [
@@ -111,12 +123,15 @@ struct Patterns: Phases {
         st10_comment,
         st11_comment,
         st12_comment,
-        st13_comment
+        st13_comment,
+        st14_comment,
+        st15_comment,
+        st16_comment
     ]
     
     
     static let st1_title = "1. Точки (Вишенка)"
-    static let st1_description = """
+    static let st1_descr = """
     <p>Еще больше узоров вы можете найти на сайте <a href=https://speedcubing.ru/tutorial/3x3x3/patterns>speedcubing.ru</a>.</p>
 
         <p><a href="rg2://ytplay?time=1:01&link=l6V7517N_lQ">Первый узор</a>
@@ -131,7 +146,7 @@ struct Patterns: Phases {
     static let st1_comment = "S\' M\' S M"
     
     static let st2_title = "2. Шахматы"
-    static let st2_description = """
+    static let st2_descr = """
     <p><a href="rg2://ytplay?time=1:30&link=l6V7517N_lQ">Второй узор,</a> пожалуй еще проще
         первого, если выполнять, через вращение средних слоев.
         А смотрится, пожалуй, даже эффектнее предыдущего. Просто вращаем каждый средний слой по два раза
@@ -157,7 +172,7 @@ struct Patterns: Phases {
     static let st2_comment = "M2 S2 E2"
     
     static let st3_title = "3. Кубики"
-    static let st3_description = """
+    static let st3_descr = """
     <p>Непростой, но очень эффектный узор. Создается впечатление, что внутри кубика 3х3 есть
         <a href="rg2://ytplay?time=3:23&link=l6V7517N_lQ">еще один кубик 2x2.</a>
         </p>
@@ -183,7 +198,7 @@ struct Patterns: Phases {
     static let st3_comment = "U2 F2 R2 U\' L2 D B R\' B R\' B R\' D\' L2 U\'"
     
     static let st4_title = "4. Змеи"
-    static let st4_description = """
+    static let st4_descr = """
     <p>Еще один очень красивый узор. <a href="rg2://ytplay?time=5:57&link=l6V7517N_lQ">Змейка</a> через весь кубик.</p>
         <p style="text-align:center"><img src="pattern_4.xml" width="20%%"></p>
         <p><b><a href="rg2://ytplay?time=6:27&link=l6V7517N_lQ">Алгоритм:</a> U B2 L D B\' F L\' D  U\' L\' R F\' D2 R\'</b></p>
@@ -196,7 +211,7 @@ struct Patterns: Phases {
     static let st4_comment = "U B2 L D B\' F L\' D  U\' L\' R F\' D2 R\'"
     
     static let st5_title = "5. Кресты"
-    static let st5_description = """
+    static let st5_descr = """
     <p><a href="rg2://ytplay?time=7:34&link=l6V7517N_lQ">Крест</a> на каждой стороне кубика.
         </p>
     <p style="text-align:center"><img src="pattern_5.xml" width="20%%"></p>
@@ -210,7 +225,7 @@ struct Patterns: Phases {
     static let st5_comment = "L2 R\' B2 F2 D2 B2 F2 L2 R2 U2 R\'"
     
     static let st6_title = "6. Молния в точку"
-    static let st6_description = """
+    static let st6_descr = """
     <p>Очень интересный и <a href="rg2://ytplay?time=8:58&link=l6V7517N_lQ">необычный узор,</a>
         которого вы не найдете на сайте <a href=https://speedcubing.ru/tutorial/3x3x3/patterns>speedcubing.ru</a>.
         Его мне прислал Дмитрий Константинов, за что ему большое спасибо.</p>
@@ -221,7 +236,7 @@ struct Patterns: Phases {
     static let st6_comment = "(R2 F B\' R2 U\'D)3"
     
     static let st7_title = "7. Уголки"
-    static let st7_description = """
+    static let st7_descr = """
     <p>Еще одна серия узоров - <a href="rg2://ytplay?time=9:50&link=l6V7517N_lQ">Уголки.</a><br/>
         Первый уголок:</p>
     <p style="text-align:center"><img src="pattern_7.xml" width="20%%"></p>
@@ -240,7 +255,7 @@ struct Patterns: Phases {
     static let st7_comment = "F2 R2 D R2 D U F2 D\' R\' D\' F L2 F\' D R U\'"
     
     static let st8_title = "8. Буквы П"
-    static let st8_description = """
+    static let st8_descr = """
     <p>Для начала <a href="rg2://ytplay?time=12:17&link=l6V7517N_lQ">короткий вариант \"Буквы П\",</a>
         в котором цвета на одной грани противоположны друг другу.</p>
     <p style="text-align:center"><img src="pattern_8.xml" width="20%%"></p>
@@ -267,27 +282,36 @@ struct Patterns: Phases {
     static let st8_comment = "((R U R\' U\') y)7"
     
     static let st9_title = "9. Полоски"
-    static let st9_description = """
+    static let st9_descr = """
     <p>Еще одна большая группа узоров - <a href="rg2://ytplay?time=13:21&link=l6V7517N_lQ">\"Полоски\".</a>
-        Немного больше вы можете найти на сайте <a href=https://speedcubing.ru/tutorial/3x3x3/patterns>speedcubing.ru.</a>
-        Начнем с очень простого варианта, который мне прислал Егор.</p>
-        <p style="text-align:center"><img src="pattern_9.xml" width="20%%"></p>
-        <p><b><a href="rg2://ytplay?time=12:55&link=l6V7517N_lQ">Алгоритм:</a> R2 F2 M2 B2 L2</b></p>
+        Немного больше вы можете найти на сайте <a href=https://speedcubing.ru/tutorial/3x3x3/patterns>speedcubing.ru.</a> Начнем с очень простого варианта, который мне прислал Егор.</p>
+    <p style="text-align:center"><img src="pattern_9.xml" width="20%%"></p>
+    
+    <p><b><a href="rg2://ytplay?time=12:55&link=l6V7517N_lQ">Алгоритм:</a> R2 F2 M2 B2 L2</b></p>
 
-        <p>Все полоски <a href="rg2://ytplay?time=13:43&link=l6V7517N_lQ">разного цвета:</a></p>
-        <p style="text-align:center"><img src="pattern_9_1.xml" width="20%%"></p>
-        <p><b><a href="rg2://ytplay?time=13:49&link=l6V7517N_lQ">Алгоритм:</a> (R2 F D R2 D\' R B2 R\' D R2 D\' F\')
-        (R2 B2) y2 (R2 F D R2 D\' R B2 R\' D R2 D\' F\') (L2 F2) M2 S2</b></p>
+    <p>Все полоски <a href="rg2://ytplay?time=13:43&link=l6V7517N_lQ">разного цвета:</a></p>
+    
+    <p style="text-align:center"><img src="pattern_9_1.xml" width="20%%"></p>
+    <p><b><a href="rg2://ytplay?time=13:49&link=l6V7517N_lQ">Алгоритм:</a> (R2 F D R2 D\' R B2 R\' D R2 D\' F\') (R2 B2) y2 (R2 F D R2 D\' R B2 R\' D R2 D\' F\') (L2 F2) M2 S2</b></p>
 
-        <p>А можно сделать и полоски на всех <a href="rg2://ytplay?time=15:22&link=l6V7517N_lQ">шести гранях</a></p>
-        <p style="text-align:center"><img src="pattern_9_2.xml" width="20%%"></p>
-        <p><b><a href="rg2://ytplay?time=15:25&link=l6V7517N_lQ">Алгоритм:</a> U\' B2 L2 U M2 U\' R2 F2 D F B R E R\' B\' R\' E R2 E\' R\' F\' B2 R2 B2 F2 S</b></p>
+    <p>А можно сделать и полоски на всех <a href="rg2://ytplay?time=15:22&link=l6V7517N_lQ">шести гранях</a></p>
+    
+    <p style="text-align:center"><img src="pattern_9_2.xml" width="20%%"></p>
+    <p><b><a href="rg2://ytplay?time=15:25&link=l6V7517N_lQ">Алгоритм:</a> U\' B2 L2 U M2 U\' R2 F2 D F B R E R\' B\' R\' E R2 E\' R\' F\' B2 R2 B2 F2 S</b></p>
+
+    <p>Ну и раздел с полосками был бы неполным без простого алгоритма для создания флага РФ на одной из сторон кубика, хотя на других сторонах при этом и получается какой-то бред.</p>
+
+    <p>Кубик надо держать стандартно, т.е. зеленой к себе, белой вверх. Получится Российский флаг на верхней стороне кубика. Спасибо Ренату Каримову за идею.</p>
+
+    <p style="text-align:center"><img src="pattern_9_3.xml" width="20%%"></p>
+
+    <p>Алгоритм: <b>M D\' F L2 F\' L</b></p>
     """
     static let st9_url = "l6V7517N_lQ"
     static let st9_comment = "R2 F2 M2 B2 L2"
     
     static let st10_title = "10. Клумба и ..."
-    static let st10_description = """
+    static let st10_descr = """
     <p>Подобных узоров, которые получаются при помощи только двойных вращений каких-либо граней,
         существует огромное множество. Все их, к сожалению, в программе привести нет никакой возможности,
         поэтому, приведу лишь один, очень <a href="rg2://ytplay?time=16:15&link=l6V7517N_lQ">простой узор,</a>
@@ -301,7 +325,7 @@ struct Patterns: Phases {
  
     
     static let st11_title = "11. Рыбки"
-    static let st11_description = """
+    static let st11_descr = """
     <p>Сначала об узоре с очень <a href="rg2://ytplay?time=16:44&link=l6V7517N_lQ">необчной рыбкой.</a>
         Кто-то в нем видит буквамы С, Р, Т и Г на боковых гранях, но если присмотреться, то буквы Т и Р образуют на грани кубика
         \"Рыбку\", поэтому авторское название узора - \"Рыбка на грани\". Спасибо Дмитрию Константинову за присланный узор.</p>
@@ -317,7 +341,7 @@ struct Patterns: Phases {
     static let st11_comment = "D L\' R\' U2 L R D"
     
     static let st12_title = "12. ФБК и Буквы Н"
-    static let st12_description = """
+    static let st12_descr = """
     <p>Очень простой узор, <a href="rg2://ytplay?time=18:00&link=l6V7517N_lQ">буквы Н</a>
         только на двух гранях. А если взять кубик стандартно,
         белой вверх, зеленой к себе, то получится эмблема ФБК. Спасибо Виктору Рудневу за присланный узор.</p>
@@ -336,29 +360,60 @@ struct Patterns: Phases {
     static let st12_url = "l6V7517N_lQ"
     static let st12_comment = "U2 F\' R2 D2 L2 B2 U2 L2 D2 F\' U2"
     
-    static let st13_title = "13. Свой алгоритм/узор"
-    static let st13_description = """
+    static let st13_title = "13. Реверс"
+    static let st13_descr = """
+    <p>Ситуация, когда все элементы (ребра и углы) находятся на своих местах, но все ребра перевернуты. Узором такую ситуацию можно назвать лишь \"с натяжкой\", но есть у этого узора одна \"фишка\" - это единственная ситуация в кубике Рубика 3х3, которую НЕВОЗМОЖНО решить, меньше чем за 20 ходов. Любую другую ситуацию можно решить за 19 ходов или меньше.</p>
+    
+    <p style="text-align:center"><img src="pattern_13.xml" width="20%%"></p>
+    
+    <p><b>Алгоритм: U R U2 R F2 L U2 R F\' B\' R2 D R\' L U2 F2 D2 F R2 D</b></p>
+    """
+    static let st13_url = "l6V7517N_lQ"
+    static let st13_comment = "U R U2 R F2 L U2 R F\' B\' R2 D R\' L U2 F2 D2 F R2 D"
+    
+    static let st14_title = "14. Подарок с бантиком"
+    static let st14_descr = """
+    <p>Еще один узор от пользователя программы Old Foxy. Напоминает красивую полосатую коробку перевязанную ленточкой с бантиком, отсюда и название.</p>
+    
+    <p style="text-align:center"><img src="pattern_14.xml" width="20%%"></p>
+    
+    <p><b>Алгоритм: B2 L2 U F2 U\' F2 R2 D\' R2 B2 F\' L\' U L2 U\' L F U\'</b></p>
+    """
+    static let st14_url = "l6V7517N_lQ"
+    static let st14_comment = "B2 L2 U F2 U\' F2 R2 D\' R2 B2 F\' L\' U L2 U\' L F U\'"
+    
+    static let st15_title = "15. Цветомузыка"
+    static let st15_descr = """
+    <p>Еще один не совсем узор, а скорее просто интересная ситуация - ни на одной стороне нет ни одного блока.</p>
+    <p style="text-align:center"><img src="pattern_15.xml" width="20%%"></p>
+    
+    <p>Алгоритм: <b>(F B L R) (F B L R) L R</b></p>
+    """
+    static let st15_url = "l6V7517N_lQ"
+    static let st15_comment = "(F B L R)2 L R"
+    
+    static let st16_title = "16. Свой алгоритм/узор"
+    static let st16_descr = """
     <p>Если у вас есть свой алгоритм для какого-то из выше приведенных узоров, то всегда можете
         написать его в поле \"Свой комментариий (алгоритм):\" к соответствующему этапу (ниже видео)
         и он отобразится в списке.</p>
 
-        <p>А если у вас есть какой-то узор и:<br/><br/>
+    <p>А если у вас есть какой-то узор и:<br/><br/>
         1. Узора <b>нет на сайте <a href=https://speedcubing.ru/tutorial/3x3x3/patterns>speedcubing.ru</a></b><br/>
         2. Этого узора нет в программе<br/>
         3. В узоре БОЛЬШЕ 6 ходов<br/>
-        4. Это <b>НЕ простой</b> узор состоящий только из двойных ходов, типа (R2 U2 L2 U2)3<br/>
-        5. Это <b>НЕ вариации</b> различных \"Букв Н\" или \"Крестов\" - поверьте, что их сделали
-        все, кому не лень было покрутить кубик<br/>
-        6. Узор состоит <b>НЕ из других</b> простых узоров.<br/><br/>
+        4. Это <b>НЕ простой</b> узор состоящий только из двойных ходов, типа (R2 U2 L2 U2)3 или
+        вращений противоположных граней (R L F B)3<br/>
+        5. Это <b>НЕ вариации</b> различных \"Букв Н\" или \"Крестов\"<br/>
+        6. Узор состоит <b>НЕ из других</b> простых узоров.<br/>
+        7. В узоре меняются местами НЕ ТОЛЬКО ребра, но и углы.<br/><br/>
         то присылайте его мне на почту
         <a href="mailto:rubicsguide@yandex.ru?subject=Узоры&body=Здравствуйте,%20Антон.">rubicsguide@yandex.ru</a><br/>
-        И если ваш узор действительно интересный, я его обязательно добавлю в программу.</p>
+        И если ваш узор <b>действительно интересный</b>, я его обязательно добавлю в программу.</p>
 
-        <p>Поймите, что эти требования появились не зря, и придумать простой узор, может каждый, кто хоть
-        немного увлекается этой темой. Но таких простых узоров десятки, если не сотни и все их просто
-        невозможно добавить в программу.
-        </p>
+    <p>P.S. Поймите, что эти требования появились не зря, и придумать простой узор, может каждый, кто хоть немного увлекается этой темой. К сожалению 99% присланных мне узоров - это простые реберные узоры, которые я не добавляю в программу. И таких простых узоров десятки, если не сотни и все их просто невозможно добавить в программу.
+    </p>
     """
-    static let st13_url = "l6V7517N_lQ"
-    static let st13_comment = ""
+    static let st16_url = "l6V7517N_lQ"
+    static let st16_comment = ""
 }
